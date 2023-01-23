f0bb565f62fab99a9582922baa7452eb0f1f1401
gaetano-amoroso <gaetanoamoroso0@gmail.com>
2023-01-23 17:54:57 +0100

    Hello world Android

---
+++ HelloWorld/app/src/main/java/com/gaetanoamoroso/corsoandroid/helloword/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gaetanoamoroso.corsoandroid.helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String LOG_TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Hello World App");
+    }
+}
